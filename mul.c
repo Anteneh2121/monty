bb81704dd1efb0ee8487b9807e3af993fe0dbc16
Anteneh Bizuneh <antukassaw1@gmail.com>
2021-12-02 09:26:03 -0800

    Implement the mul opcode.

--- mul.c
+++ mul.c
@@ -5,6 +5,7 @@
  * @head: top of the stack
  * @line_number: line number where a wrong command was found
  * Return: void
+ * author Anteneh Bizuneh for Alx project
  */
 void _mul(stack_t **head, unsigned int line_number)
 {
